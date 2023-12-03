7dd131b0b9219cc284ebcc3ccd82e464878612d8
fcortazar <fcd0005@alu.medac.es>
2023-12-03 19:44:02 +0100

    Origen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,4 @@
-import java.io.FileReader;
-import java.io.IOException;
-import java.sql.*;
-import java.util.Properties;
-import java.util.Scanner;
-
 public class Main {
-
-    private static Connection connection;
-
     public static void main(String[] args) {
-        cargarConfiguracion();
-
-        try {
-            Class.forName(Configuracion.getProperty("db.driver"));
-            connection = DriverManager.getConnection(
-                    Configuracion.getProperty("db.url"),
-                    Configuracion.getProperty("db.usuario"),
-                    Configuracion.getProperty("db.clave")
-            );
-
-            while (true) {
-                mostrarMenu();
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
     }
-
-    private static void cargarConfiguracion() {
-        Properties propiedades = new Properties();
-        try (FileReader reader = new FileReader("src/main/resources/file.properties")) {
-            propiedades.load(reader);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Configuracion.setProperties(propiedades);
-    }
-
-    // Resto del código...
 }
